4b3c1db340a3223caa87af13cf0bc6da656a7c03
JuanDomingoVrgn <alejousetuni@gmail.com>
2021-03-06 20:26:12 -0300

    agregue foto

--- screens/FMAScreen.tsx
+++ screens/FMAScreen.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, ImageBackground } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
@@ -7,9 +7,12 @@ import { Text, View } from '../components/Themed';
 export default function FMAScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>FRANJA MORADA APP</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/FMAScreen.tsx" />
+      <ImageBackground  style= { styles.backgroundImage } source= {require("../assets/images/APPFM.png")}>
+        <Text style={styles.title}>FRANJA MORADA APP12341141241</Text>
+        <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+        {/* <EditScreenInfo path="/screens/FMAScreen.tsx" /> */}
+      </ImageBackground>
+      
     </View>
   );
 }
@@ -29,4 +32,11 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
+  backgroundImage:{
+    flex: 1,
+    resizeMode: 'center', // or 'sctreth'
+    
+    height: '80%' ,
+          width: '100%'
+    }
 });
